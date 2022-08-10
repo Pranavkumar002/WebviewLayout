8cad9664fdccdbf55bc38657e3e8ff6f36c81acc
Pranavkumar002 <pranav2002kashyap@gmail.com>
2022-08-10 12:41:08 +0530

    c

--- app/src/main/java/com/pranavkumar/webviewlayout/MainActivity.kt
+++ app/src/main/java/com/pranavkumar/webviewlayout/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.pranavkumar.webviewlayout
 
+import android.app.Activity
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     lateinit var Webview: WebView
     lateinit var WebViewClient:WebViewClient
+    lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
@@ -17,12 +23,19 @@ class MainActivity : AppCompatActivity() {
         WebViewClient= WebViewClient()
         Webview.loadUrl("https://www.google.com")
         Webview.setWebViewClient(object : WebViewClient(){
+
             override fun shouldOverrideUrlLoading(
                 view: WebView?,
                 request: WebResourceRequest?
             ): Boolean {
-
                 return super.shouldOverrideUrlLoading(view, request)
+            }
+
+
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBar.visibility=View.GONE
             }
         })
         Webview.settings.javaScriptEnabled = true
